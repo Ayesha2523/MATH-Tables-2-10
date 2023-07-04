88f942ad01504d7211bcc50776b828e0f23d66c3
Ayesha2523 <111120173+Ayesha2523@users.noreply.github.com>
2023-07-04 20:03:42 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,77 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+       
+    cin>>a;
+    
+    cout<<a;
+    cout<<"x1=";
+    cout<<a*1;
+    cout<<"\n";     
+    
+    
+    cout<<a;
+    cout<<"x2=";
+    cout<<a*2;
+    cout<<"\n";
+    
+    cout<<a;
+    cout<<"x3=";
+    cout<<a*3;
+    cout<<"\n";
+   
+    cout<<a;
+    cout<<"x4=";
+    cout<<a*4; 
+    cout<<"\n";
+    
+    cout<<a;
+    cout<<"x5=";
+    cout<<a*5; 
+    cout<<"\n";
+    
+    
+    cout<<a;
+    cout<<"x6=";
+    cout<<a*6; 
+    cout<<"\n";
+    
+    cout<<a;
+    cout<<"x7=";
+    cout<<a*7; 
+    cout<<"\n";
+    
+    cout<<a;
+    cout<<"x8=";
+    cout<<a*8; 
+    cout<<"\n";
+    
+    
+    cout<<a;
+    cout<<"x9=";
+    cout<<a*9; 
+    cout<<"\n";
+    
+    cout<<a;
+    cout<<"x10=";
+    cout<<a*10; 
+    cout<<"\n";
+    
+    
+    
+
+    
+}
